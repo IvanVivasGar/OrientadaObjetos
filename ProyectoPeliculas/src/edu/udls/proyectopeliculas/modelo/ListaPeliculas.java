b4ae380323feab7a2dff8c8abed400a7044c6cc1
Ivan Vivas Garcia <ivg77850@lasallebajio.edu.mx>
2023-05-09 12:41:07 -0600

    Actualizacion Proyecto Peliculas

--- ProyectoPeliculas/src/edu/udls/proyectopeliculas/modelo/ListaPeliculas.java
+++ ProyectoPeliculas/src/edu/udls/proyectopeliculas/modelo/ListaPeliculas.java
@@ -9,5 +9,43 @@ package edu.udls.proyectopeliculas.modelo;
  * @author Ivan
  */
 public class ListaPeliculas {
+    Nodo first;
+    Nodo actual;
+    Nodo last;
+    int size;
     
+    public ListaPeliculas(){}
+    
+    public int size(){
+        return this.size;
+    }
+    
+    public String first(){
+        return this.first.getPelicula();
+    }
+    
+    public String last(){
+        return this.last.getPelicula();
+    }
+    
+    public String actual(){
+        return this.actual.getPelicula();
+    }
+    
+    public String search(int i){
+        String msg;
+        if(i <= this.size()){
+            actual = first;
+            for(int z = 0; z < i; z++){
+                actual = actual.getSiguiente();
+            }
+            msg = actual.getPelicula();
+        }else{
+            msg = "La posicion solicitada no existe";
+        }
+        
+        return msg;
+    }
+    
+    public 
 }
