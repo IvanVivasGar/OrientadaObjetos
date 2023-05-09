30ddd80fb302829c0dc8b40f95c2f78a816162b9
ivanvivasga <114440414+ivanvivasga@users.noreply.github.com>
2023-05-09 10:46:42 -0600

    Actualizacion Nodo

--- ProyectoPeliculas/src/edu/udls/proyectopeliculas/modelo/Nodo.java
+++ ProyectoPeliculas/src/edu/udls/proyectopeliculas/modelo/Nodo.java
@@ -9,5 +9,33 @@ package edu.udls.proyectopeliculas.modelo;
  * @author Ivan
  */
 public class Nodo {
+    private String pelicula;
+    private Nodo anterior;
+    private Nodo siguiente;
     
+    public Nodo(){}
+    
+    public void setPelicula(String pelicula){
+        this.pelicula = pelicula;
+    }
+    
+    public String getPelicula(){
+        return this.pelicula;
+    }
+    
+    public void setAnterior(Nodo anterior){
+        this.anterior = anterior;
+    }
+    
+    public Nodo getAnterior(){
+        return this.anterior;
+    }
+    
+    public void setSiguiente(Nodo Siguiente){
+        this.siguiente = siguiente;
+    }
+    
+    public Nodo getSiguiente(){
+        return this.siguiente;
+    }
 }
